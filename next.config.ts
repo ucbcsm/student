07d15a006bfd10926cbfa5c26d9dbb618bb052c7
Kahindo Lwanzo Alfred <alfredlwanzo@gmail.com>
2025-06-16 04:47:56 -0700

    Fix Eslint: ignoreDuringBuilds: true

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
